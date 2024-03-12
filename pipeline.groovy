bb499493ab80305d9a8230405591750fe647c3af
Evgeniy Kornyshev <Evgeniy_Kornyshev@epam.com>
2024-03-12 17:17:18 +0300

    Jenkins Pipeline v2

--- pipeline.groovy
+++ pipeline.groovy
@@ -5,16 +5,16 @@ pipeline {
         string(name: 'TAF_BRANCH', defaultValue: 'master', description: 'Branch of Test Automation Framework', trim: true)
     }
 
+    options {
+        timestamps()
+    }
 
     stages {
         stage('Checkout from GitHub') {
             steps {
-                echo ''
-                 checkout([
-                     $class: 'GitSCM',
-                     branches: [[name: "*/${params['TAF_BRANCH']}'"]],
-                     userRemoteConfigs: [[url: 'https://github.com/Kornyshev/taf-template-bdd-selenide-rest']]
-                 ])
+                cleanWs()
+                echo "Checkout in workspace: ${WORKSPACE}"
+                git branch: "${TAF_BRANCH}", url: 'https://github.com/Kornyshev/taf-template-bdd-selenide-rest'
             }
         }
 
@@ -24,7 +24,7 @@ pipeline {
                         string(credentialsId: 'user.email', variable: 'USER_EMAIL'),
                         string(credentialsId: 'user.password', variable: 'USER_PASSWORD'),
                         string(credentialsId: 'api.token', variable: 'API_TOKEN')]) {
-                    bat "mvn clean test -Duser.email=${USER_EMAIL} -Duser.password=${USER_PASSWORD} -Dapi.token=${API_TOKEN} -Dselenide.headless=true"
+                    sh "mvn clean test -Duser.email=${USER_EMAIL} -Duser.password=${USER_PASSWORD} -Dapi.token=${API_TOKEN} -Dselenide.headless=true"
                 }
             }
         }
